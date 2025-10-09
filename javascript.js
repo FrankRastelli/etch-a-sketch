f833bbb768c5f927dec18e897ff4b09a8b83d7b0
Frank Rastelli <frankice8910@gmail.com>
2025-10-09 10:35:12 -0400

    Add mode switch functionality and clear function
    added rainbow and fixed color mode

--- javascript.js
+++ javascript.js
@@ -1,20 +1,34 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridBtn = document.querySelector(".input");
+const randomBtn = document.querySelector(".random");
 const colorBtn = document.querySelector(".color");
 const darkeningBtn = document.querySelector(".darkening");
 const clearBtn = document.querySelector(".clear");
 
+let gridSize = 16;
+let mode = "random";
+let fixedColor = "white";
+//something for darkening
+
 // create base 16x16 grid
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < (gridSize * gridSize); i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
 
     grid.addEventListener("mouseenter", () => {
-    let color = getRandomRgbColor();
-    grid.style.backgroundColor = color;
+        colorMode(grid);
     });
 
     gridContainer.appendChild(grid);
+}
+
+function colorMode(squares) {
+    if (mode === "random") {
+        squares.style.backgroundColor = getRandomRgbColor();
+    }
+    else if (mode === "fixed") {
+        squares.style.backgroundColor = fixedColor;
+    }
 }
 
 function getRandomRgbColor() {
@@ -32,6 +46,7 @@ gridBtn.addEventListener("click", () => {
     }
 
     let newGridSize = parseInt(numberOfSquares);
+    gridSize = newGridSize;
 
     // remove old grid
     while (gridContainer.firstChild) {
@@ -44,8 +59,7 @@ gridBtn.addEventListener("click", () => {
         grid.classList.add("grid");
 
         grid.addEventListener("mouseenter", () => {
-        let color = getRandomRgbColor();
-        grid.style.backgroundColor = color;
+            colorMode(grid);
         });
 
         let size = 640 / newGridSize + "px";
@@ -56,18 +70,45 @@ gridBtn.addEventListener("click", () => {
     }
 });
 
+randomBtn.addEventListener("click", () => {
+    mode = "random";
+});
+
 colorBtn.addEventListener("click", () => {
     let newColor = prompt("Enter a color you want to use");
+    if (newColor === null) return;
 
     if (CSS.supports("color", newColor)) {
-        grid.addEventListener("mouseenter", () => {
-            grid.forEach(square => {
-                square.style.backgroundcolor = newColor;
-            });
-        });
+        fixedColor = newColor;
+        mode = "fixed";
     }
     else {
         alert("Invalid color");
         return;
     }
 });
+
+// darkening btn ******
+
+clearBtn.addEventListener("click", () => {
+    // remove old grid
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    // create new grid
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+
+        grid.addEventListener("mouseenter", () => {
+            colorMode(grid);
+        });
+
+        let size = 640 / gridSize + "px";
+        grid.style.height = size;
+        grid.style.flex = `0 0 ${size}`;
+
+        gridContainer.appendChild(grid);
+    }
+})
